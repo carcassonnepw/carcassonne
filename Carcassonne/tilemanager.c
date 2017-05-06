e0e72e6b982ea9c483e4710534ce7bc3a835508c
6emmes <q@zakrzewscy.com>
2017-05-06 13:47:52 +0200

    Update tilemanager.c

--- Carcassonne/tilemanager.c
+++ Carcassonne/tilemanager.c
@@ -22,13 +22,11 @@ void rotation(int GID, int times)
 	int temp1, temp2,temp3;
 	int tileid = tilesongrid[GID].ID;
 	for(i=times;i>0;i--)
-	{
+	{		
 		temp1= tiles[tileid].right;
-		tiles[tileid].right=tiles[tileid].top;
-		temp2=tiles[tileid].bottom;
-		temp3=tiles[tileid].left;
-		tiles[tileid].bottom=temp1;
-		tiles[tileid].left=temp2;
-		tiles[tileid].top=temp3;
+		tiles[tileid].right = tiles[tileid].top;
+		tiles[tileid].top = tiles[tileid].left;
+		tiles[tileid].left = tiles[tileid].bottom;
+		tiles[tileid].bottom = temp1;
 	}
 }
