49cba140a4ae4be96b9c4c39f2dee167f8752d95
6emmes <q@zakrzewscy.com>
2017-05-09 18:24:11 +0200

    void->int placetile

--- Carcassonne/gridmanager.h
+++ Carcassonne/gridmanager.h
@@ -5,7 +5,7 @@ extern int grid[GRIDSIZE][GRIDSIZE];
 extern int gids; 
 
 int checkisempty(int x, int y);
-void placetile(int id, int x, int y);
+int placetile(int id, int x, int y);
 void showgrid(int grid[30][30]);
 void showtilesongrid();
 #endif
